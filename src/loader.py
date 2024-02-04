5aa0194a505ab3c704a7286bad0d794919d54822
Jia Wei Leong <60588144+JWL16038@users.noreply.github.com>
2024-02-04 19:44:11 +1300

    Add function to generate a JSON metadata file for all docs

--- src/loader.py
+++ src/loader.py
@@ -1,3 +1,7 @@
+import json
+import os
+import uuid
+import PyPDF2
 from glob import glob
 from pathlib import Path
 from langchain.text_splitter import RecursiveCharacterTextSplitter, TokenTextSplitter
@@ -21,11 +25,39 @@ token_splitter = TokenTextSplitter(
     encoding_name="cl100k_base"
 )
 
+def generate_or_update_metadata(pdf_files):
+    """
+    Generates a JSON metadata file to store the required contents for all PDF documents. These include:
+    - The filename of the PDF
+    - The size in MBs
+    - The page count of the document
+    """
+    metadata_path = FULL_DOCS_PATH.joinpath("metadata.json").as_posix()
+    with open(metadata_path, "w") as file:
+        output = []
+        for pdf in pdf_files:
+            metadata = {}
+            doc_data = {}
+            id = str(uuid.uuid4())
+            name = Path(pdf).stem
+            file_stats = os.stat(pdf)
+            size_mb = round(file_stats.st_size / (1024 * 1024), 3)
+            page_count = len(PyPDF2.PdfReader(pdf).pages)
+            doc_data["filename"] = name
+            doc_data["size_mb"] = size_mb
+            doc_data["page_count"] = page_count
+            metadata["id"] = id
+            metadata["content"] = doc_data
+            print(metadata)
+            output.append(metadata)
+        json.dump(output, file, indent=2)
+
 def load_documents(db="chroma", recursive=True):
     """
     Load all PDF document and insert the chunks into a database
     """
     pdf_files = glob(str(FULL_DOCS_PATH.joinpath("**/*.pdf")), recursive=recursive)
+    generate_or_update_metadata(pdf_files)
     data = [PyMuPDFLoader(FULL_DOCS_PATH.joinpath(pdf).as_posix()).load() for pdf in pdf_files]
     docs = [doc for d in data for doc in text_splitter.split_documents(d)]
     if db == "chroma":
